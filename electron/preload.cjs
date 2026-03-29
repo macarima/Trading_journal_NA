5a0246fa61b45e7106f96e8a0d33a95e7e32d847
Hui-Su Kim <macarima@gmail.com>
2026-03-30 01:24:23 +0900

    Update preload.cjs

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -4,6 +4,7 @@ contextBridge.exposeInMainWorld('api', {
   loadTrades: () => ipcRenderer.invoke('load-trades'),
   saveTrades: (records) => ipcRenderer.invoke('save-trades', records),
   fetchMarketData: (date) => ipcRenderer.invoke('fetch-market-data', date),
+  fetchMonthlyIndex: (year, month) => ipcRenderer.invoke('fetch-monthly-index', year, month),
   exportPDF: (html) => ipcRenderer.invoke('export-pdf', html),
   importTsv: () => ipcRenderer.invoke('import-tsv'),
 });
